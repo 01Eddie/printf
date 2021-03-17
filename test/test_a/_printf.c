3a1d4ded9c8f571a0ba0e472cab3f7df25d3659f
adrs <ra.condezomonge@gmail.com>
2021-03-16 22:19:19 -0500

    [Delete] 0-printf.c printf

--- test/test_a/_printf.c
+++ test/test_a/_printf.c
@@ -1,61 +1,59 @@
 #include "holberton.h"
- 
- /**
- * _printf - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+
+/**
+ * _printf - function principal for functionality of print
+ * @format: Character
+ * Return: 0 or len
  */
-int _printf(const char* format,...) 
+
+int _printf(const char *format, ...)
 {
-    unsigned int i, j;
-    unsigned int len = 0; /* outFlag = 0,  */ 
-    //char *s; 
-    //char *traverse;
-    printT print[] = {
-        {"c", p_char},
-        {"s", p_str},
+	unsigned int i, j;
+	unsigned int len = 0;/* outFlag = 0,*//*char *s;*//*char *traverse;*/
+	printT print[] = {
+		{"c", p_char},
+		{"s", p_str},
+		{"d", p_doub},
+		{"i", p_int},
+		{NULL, NULL}
+	};/*Module 1: Initializing Myprintf's arguments*/
 
-        {NULL, NULL}
-    };//Module 1: Initializing Myprintf's arguments 
-    va_list arg; 
-    va_start(arg, format); 
+	va_list arg;
 
-    if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-        return 0;
+	va_start(arg, format);
 
-    i = 0; 
-    while(format[i] != '\0') 
-    {
-        if (format[i] == '%' && format[i + 1] != '%')
-        {
-           //outFlag = 0;
-            for(j = 0; print[j].p != NULL; j++) 
-            { //Module 2: Fetching and executing arguments
-                if (format[i + 1] == print[j].print[0])
-                {
-                    len = len + print[j].p(arg);
-                   // outFlag = 1;
-                    i++;
-                } 
-            }
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (0);
 
-        }
-        else if (format[i] == '%' && format[i + 1] == '%')
-        {
-            _putchar('%'); 
-            i++;
-            len = len + 1;
-        }
-        else
-        {
-            _putchar(format[i]);
-            len = len + 1;
-        }
-        i++;
-    } //Module 3: Closing argument list to necessary clean-up
-    va_end(arg); 
+	i = 0;
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%' && format[i + 1] != '%')
+		{/*outFlag = 0;*/
+			for (j = 0; print[j].p != NULL; j++)
+			{/*Module 2: Fetching and executing arguments*/
+				if (format[i + 1] == print[j].print[0])
+				{
+					len = len + print[j].p(arg);/*outFlag = 1;*/
+					i++;
+				}
+			}
+		}
+		else if (format[i] == '%' && format[i + 1] == '%')
+		{
+			_putchar('%');
+			i++;
+			len = len + 1;
+		}
+		else
+		{
+			_putchar(format[i]);
+			len = len + 1;
+		}
+		i++;
+	} /*Module 3: Closing argument list to necessary clean-up*/
 
-    return (len);
-} 
+	va_end(arg);
+	return (len);
+}
+
--- test/test_a/main.c
+++ test/test_a/main.c
@@ -10,20 +10,20 @@
 int main(void)
 {
 
-    /* int len; */
-    /* int len2; */
-    /* len = _printf("Let's try to printf a simple sentence.\n"); */
-    /* len2 = printf("Let's try to printf a simple sentence.\n"); */
-    /* printf("Length:[%d, %i]\n", len2, len2); */
-    /* _printf("Length:[%d, %i]\n", len, len); */
-   /* _printf("Negative:[%d]\n", -762534); */
-   /*  printf("Negative:[%d]\n", -762534); */
+    int len;
+    int len2;
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Length:[%d, %i]\n", len, len);
+   _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
  	_printf("Character:[%c]\n", 'H');
     printf("Character:[%c]\n", 'H');
     _printf("String:[%s]\n", "I am a string !");
     printf("String:[%s]\n", "I am a string !");
-	/* _printf("Len:[%d]\n", len); */
-    /* printf("Len:[%d]\n", len2); */
+	_printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
     return 0;
 
     /* int len; */
