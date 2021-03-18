1ad7af23af346778fcbe5e3caa3827a298f0bf2a
01Eddie <jesus.11.97@hotmail.com>
2021-03-18 17:36:35 -0500

    [modified] new variable outFlag, that modified the flag '%'

--- _printf.c
+++ _printf.c
@@ -8,7 +8,7 @@
 
 int _printf(const char *format, ...)
 {
-	unsigned int i = 0, j, len = 0;
+	unsigned int i = 0, j, len = 0, outFlag = 1;
 	printT print[] = {{"c", p_char}, {"s", p_str}, {"d", p_doub}, {"i", p_int},
 	{"b", p_binary}, {NULL, NULL}};/*Module 1: Initializing Myprintf's arguments*/
 
@@ -16,8 +16,8 @@ int _printf(const char *format, ...)
 
 	va_start(arg, format);
 
-	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
-		return (0);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))/*format[0]==%*/
+		return (-1);
 
 	while (format != NULL && format[i] != '\0')
 	{
@@ -28,20 +28,17 @@ int _printf(const char *format, ...)
 				if (format[i + 1] == print[j].print[0])
 				{
 					len = len + print[j].p(arg);
+					outFlag = 0;
 					i++;
 				}
 			}
-		}
-		else if (format[i] == '%' && format[i + 1] == '%')
-		{
-			_putchar('%');
-			i++;
-			len = len + 1;
+			if (outFlag == 1)
+				_putchar(format[i]);/*len++;*/
 		}
 		else
 		{
 			_putchar(format[i]);
-			len = len + 1;
+			len++;
 		}
 		i++;
 	} /*Module 3: Closing argument list to necessary clean-up*/
